b16943d8d47b805e647215537bf1139fef007561
BEATRIZ JULIÃO BOLETINI DA SILVA <24122273@CFP127ALUNO.LOCAL>
2025-01-30 15:25:24 -0300

    main pronto

--- main.js
+++ main.js
@@ -1,4 +1,11 @@
 "use strict"
 
-nome = 'Beatriz Boletini'
-alert(none)
+const botaoTrocarCor = document.getElementById('trocar-cor')
+
+function trocarCor(){
+    const cor = document.getElementById('cor').value
+    document.documentElement.style.setProperty('--cor-fundo', cor)
+}
+    
+
+botaoTrocarCor.addEventListener('click', trocarCor)
